e62cab4722cf991cc61b14915162af086eb9a2b5
Thang <71360771+thangpham0311@users.noreply.github.com>
2021-05-04 16:25:15 +0700

    set up

--- App.tsx
+++ App.tsx
@@ -8,7 +8,7 @@
  * @format
  */
 
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import {
   Alert,
   Dimensions,
@@ -19,46 +19,48 @@ import {
   StatusBar,
   StyleSheet,
   Text,
+  TextInput,
   TouchableOpacity,
   useColorScheme,
   View,
 } from 'react-native';
 import viewSanPham from './src/viewSanPham';
+import _ from 'lodash';
 
 const DATA = [
   {
     id: 1,
-    title: 'HP Pavilion Gaming Laptop',
+    title: 'lenovo',
     price: 1000.00,
     img: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcT5eqK_mbxJ8X2ch7mkPygAc1aJs4pGe-FCkA&usqp=CAU'
   },
   {
     id: 2,
-    title: 'HP Pavilion Gaming Laptop',
+    title: 'dell',
     price: 1000.00,
     img: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTo-EX5nW0bS69BUjwU2kzKPm7nqIC0sdu5Eg&usqp=CAU'
   },
   {
     id: 3,
-    title: 'HP Pavilion Gaming Laptop',
+    title: 'asus',
     price: 1000.00,
     img: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRLLuo4Oq3eov8b3nFnpq1xNIFJopLLkvrQ9A&usqp=CAU'
   },
   {
     id: 4,
-    title: 'HP Pavilion Gaming Laptop',
+    title: 'apple',
     price: 1000.00,
     img: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTo-EX5nW0bS69BUjwU2kzKPm7nqIC0sdu5Eg&usqp=CAU'
   },
   {
     id: 5,
-    title: 'HP Pavilion Gaming Laptop',
+    title: 'hp',
     price: 1000.00,
     img: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTo-EX5nW0bS69BUjwU2kzKPm7nqIC0sdu5Eg&usqp=CAU'
   },
   {
     id: 6,
-    title: 'HP Pavilion Gaming Laptop',
+    title: 'dell',
     price: 1000.00,
     img: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSBCN0daqNWndzDcmUA0EcpphZODyye5nUa2A&usqp=CAU'
   }
@@ -73,6 +75,19 @@ type customItem = {
 
 
 const App = () => {
+  const [data1, setData1] = useState(DATA);
+  const [data2, setData2] = useState(DATA);
+  const [temp, setTemp] = useState('');
+
+  useEffect(() => {
+    setInterval(() => {
+      setData1(_.shuffle(data2))
+    }, 5000)
+  }, [])
+
+  useEffect(() => {
+    setData1(data2)
+  }, [data2])
 
   const Item = ({ item }: { item: customItem }) => (
     <TouchableOpacity style={styles.item}
@@ -96,9 +111,21 @@ const App = () => {
 
   return (
     <SafeAreaView style={styles.container}>
+      <View>
+        <TextInput
+          placeholder="Tìm kiếm"
+          style={{ backgroundColor: 'gray' }}
+          onChangeText={(val) => setTemp(val)}
+        />
+      </View>
+      <TouchableOpacity
+        onPress={() => { setData2(DATA.filter(name => name.title.includes(temp))) }}
+      >
+        <Text>TÌm kiếm</Text>
+      </TouchableOpacity>
       <FlatList
         numColumns={2}
-        data={DATA}
+        data={data1}
         renderItem={renderItem}
         keyExtractor={(item) => item.id.toString()}
         style={{ marginTop: 20 }}
